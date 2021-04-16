7d46a59c7134ea085ecac09454a8aa03af475814
allegiant <allegiant.xu@hotmail.com>
2021-04-16 10:00:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,8 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 
 # Add luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+
+## replace luci-theme-argon 
+cd package/lean  
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
