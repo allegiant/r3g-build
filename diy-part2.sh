9d0e2caa898439642ca5173ccef9745f3b99fb27
allegiant <allegiant.xu@hotmail.com>
2021-04-19 15:27:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,5 +23,5 @@ git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/l
 ## overclock 1000Mhz--0x312 1100Mhz--0x362 1200Mhz--0x3B2
 pushd target/linux/ramips/patches-5.4
 sed -i '68s/89/98/' 102-mt7621-fix-cpu-clk-add-clkdev.patch
-sed -i 'N;156 a +\t\tif ((pll & 0x7f0) == 0x2b0) {\n+\t\t\tvolatile u32 i;\n+\n+\t\t\tpr_info("CPU Clock: 880MHz, start overclocking\\n");\n+\t\t\tpll &= ~0x7ff;\n+\t\t\tpll |= 0x3B2;\n+\t\t\trt_memc_w32(pll, MEMC_REG_CPU_PLL);\n+\t\t\tfor (i = 0; i < 1000; i++);\n+\t\t}' 102-mt7621-fix-cpu-clk-add-clkdev.patch
+sed -i 'N;156 a +\t\tif ((pll & 0x7f0) == 0x2b0) {\n+\t\t\tvolatile u32 i;\n+\n+\t\t\tpr_info("CPU Clock: 880MHz, start overclocking\\n");\n+\t\t\tpll &= ~0x7ff;\n+\t\t\tpll |= 0x362;\n+\t\t\trt_memc_w32(pll, MEMC_REG_CPU_PLL);\n+\t\t\tfor (i = 0; i < 1000; i++);\n+\t\t}' 102-mt7621-fix-cpu-clk-add-clkdev.patch
 popd
