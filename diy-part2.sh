5d2453cccd1e0f2680203250a14d83c5398b9fed
allegiant <allegiant.xu@hotmail.com>
2021-04-21 17:59:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,8 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
+git clone --depth=1 https://github.com/fw876/helloworld package/lean/helloworld
+
 # Add luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
 
